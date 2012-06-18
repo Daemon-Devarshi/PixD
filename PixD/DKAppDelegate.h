14b4cef093dee604e62dd9471c791b7122ba1e00
Devarshi Kulshreshtha <devarshi.kulshreshtha@gmail.com>
2012-06-18 16:21:50 +0530

    Initial Commit

---
+++ PixD/DKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DKAppDelegate.h
+//  PixD
+//
+//  Created by Devarshi Kulshreshtha on 18/06/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DKAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ PixD/DKAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  DKAppDelegate.m
+//  PixD
+//
+//  Created by Devarshi Kulshreshtha on 18/06/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "DKAppDelegate.h"
+
+@implementation DKAppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
